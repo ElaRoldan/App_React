d0bd319cf99f766dc36805a6c49363de77814306
ElaRoldan <estela.roldan@gmail.com>
2018-12-13 19:48:49 -0600

    Subir proyecto de React

---
+++ src/PLAYLIST/components/media.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+class Media extends React.Component {
+// UNa clase que extiende los poderes de react
+  render () {
+  	return (
+       <div>
+         <div>
+           <img 
+             src= "" 
+             alt= ""
+             width={260}
+             height={260}
+           />
+           <h3>¿Por que aprender React?</h3>
+           <p>Autor</p>
+         </div>
+       </div>
+    )
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Media from './src/playlist/components/media';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
@@ -10,3 +11,4 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
+ReactDOM.render(<Media/> , app);
